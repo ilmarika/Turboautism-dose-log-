42f226ebf3623808e20426e42a40d0ad88593735
Emmi <ilmari.linna@gmail.com>
2026-02-22 17:57:51 +0200

    first build of dose log

--- app/src/main/java/com/example/turboautismdoselog/FirstFragment.java
+++ app/src/main/java/com/example/turboautismdoselog/FirstFragment.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.turboautismdoselog.databinding.FragmentFirstBinding;
 
@@ -17,22 +17,24 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(v ->
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
-        );
+        binding.buttonSave.setOnClickListener(v -> {
+            android.util.Log.d("MED_LOG", "BUTTON CLICKED");
+            android.widget.Toast.makeText(requireContext(),
+                    "CLICK WORKS",
+                    android.widget.Toast.LENGTH_LONG).show();
+        });
     }
 
     @Override
@@ -40,5 +42,4 @@ public class FirstFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
--- app/src/main/java/com/example/turboautismdoselog/MainActivity.java
+++ app/src/main/java/com/example/turboautismdoselog/MainActivity.java
@@ -1,77 +1,41 @@
 package com.example.turboautismdoselog;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.turboautismdoselog.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    EditText editMedication, editRoute, editDosage;
+    Button buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        editMedication = findViewById(R.id.editMedication);
+        editRoute = findViewById(R.id.editRoute);
+        editDosage = findViewById(R.id.editDosage);
+        buttonSave = findViewById(R.id.buttonSave);
 
-        setSupportActionBar(binding.toolbar);
+        buttonSave.setOnClickListener(v -> {
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            String medication = editMedication.getText().toString();
+            String route = editRoute.getText().toString();
+            String dosage = editDosage.getText().toString();
+            long timestamp = System.currentTimeMillis();
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+            Toast.makeText(this,
+                    "Saved:\n" +
+                            "Medication: " + medication + "\n" +
+                            "Route: " + route + "\n" +
+                            "Dosage: " + dosage + "\n" +
+                            "Timestamp: " + timestamp,
+                    Toast.LENGTH_LONG).show();
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
